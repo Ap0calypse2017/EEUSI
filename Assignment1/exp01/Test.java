c71e9b59054481b41457cd0877e15b715529fd2d
Alessandro Zanzi <alessandro.zanzi@usi.ch>
2023-10-31 01:18:26 +0100

    added multithreading, fixed pass by reference of arrays

--- Assignment1/exp01/Test.java
+++ Assignment1/exp01/Test.java
@@ -1,13 +1,21 @@
 package exp01;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
-
-import javax.swing.Popup;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class Test {
 
     private static int REPEAT_TIMES = 10; // Number of repetitions for a multiple benchmark test
+    private static int[] POOLS = new int[] { 100, 1000, 10000, 50000 }; // sizes of arrays to test
+
+    private static int N_CORES = Runtime.getRuntime().availableProcessors();
+    private static ExecutorService executorService = Executors.newFixedThreadPool(N_CORES * (1 + 50 / 5));
 
     /*
      * Generate a random Integer array of given size.
@@ -37,14 +45,26 @@ public class Test {
     }
 
     /*
-     * Repeat the same
+     * Repeat the same sorting algorithm multiple times.
      */
     private static <T extends Comparable<T>> long[] repeatBenchmark(Sorter<T> sorter, T[] array, int times) {
         long[] attempts = new long[times];
+        List<Future<Long>> futures = new ArrayList<Future<Long>>();
 
         for (int i = 0; i < times; i++) {
-            T[] burnerArray = Arrays.copyOf(array, array.length);
-            attempts[i] = benchmarkSort(sorter, burnerArray);
+            /* Schedule each repetition */
+            futures.add(executorService.submit(() -> {
+                return benchmarkSort(sorter, Arrays.copyOf(array, array.length));
+            }));
+        }
+
+        for (int i = 0; i < times; i++) {
+            /* Wait for scheduled tasks to finish */
+            try {
+                attempts[i] = futures.get(i).get();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
         return attempts;
@@ -63,32 +83,55 @@ public class Test {
     }
 
     public static void main(String args[]) {
+        long start, end;
+
         /* Initialize sorters */
         BubbleSortPassPerItem<Integer> ppiSorter = new BubbleSortPassPerItem<Integer>();
         BubbleSortUntilNoChange<Integer> uncSorter = new BubbleSortUntilNoChange<Integer>();
         BubbleSortWhileNeeded<Integer> wnSorter = new BubbleSortWhileNeeded<Integer>();
 
-        int[] pools = new int[] { 100, 1000, 10000, 100000, 1000000 }; // sizes of arrays to test
         System.out.println("\nRUNING TEST ON ARRAYS OF VARYING SIZES");
         System.out.printf("Each test is performed %d times\n", REPEAT_TIMES);
         System.out.println("-".repeat(55));
         System.out.format("%10s%15s%15s%15s\n", "Array Size", "PassPerItem", "UntilNoChange", "WhileNeeded");
         System.out.println("-".repeat(55));
 
+        start = System.nanoTime();
         /* Go through all test cases */
-        for (int p : pools) {
+        for (int p : POOLS) {
             /* Generate random array of size p */
             Integer[] array = generateRandomArray(p);
 
-            /* Implementation for multiple benchmarks */
-            long[] ppiAttempts = repeatBenchmark(ppiSorter, array, REPEAT_TIMES);
-            long[] uncAttempts = repeatBenchmark(uncSorter, array, REPEAT_TIMES);
-            long[] wnAttempts = repeatBenchmark(wnSorter, array, REPEAT_TIMES);
+            /* Implementation for multiple benchmarks (schedule to execute) */
+            Future<long[]> ppiFuture = executorService.submit(() -> repeatBenchmark(ppiSorter, Arrays.copyOf(array, array.length), REPEAT_TIMES));
+            Future<long[]> uncFuture = executorService.submit(() -> repeatBenchmark(uncSorter, Arrays.copyOf(array, array.length), REPEAT_TIMES));
+            Future<long[]> wnFuture = executorService.submit(() -> repeatBenchmark(wnSorter, Arrays.copyOf(array, array.length), REPEAT_TIMES));
 
-            /* Print mean benchmarks */
-            System.out.format("%10d%15d%15d%15d\n", p, meanTime(ppiAttempts), meanTime(uncAttempts),
-                    meanTime(wnAttempts));
+            /* Wait for execution termination */
+            try {
+                long[] ppiAttempts = ppiFuture.get();
+                long[] uncAttempts = uncFuture.get();
+                long[] wnAttempts = wnFuture.get();
+
+                /* Print mean benchmarks */
+                System.out.format("%-10d%15d%15d%15d\n", p, meanTime(ppiAttempts), meanTime(uncAttempts),
+                        meanTime(wnAttempts));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
+
+        end = System.nanoTime();
         System.out.println("-".repeat(55));
+        System.out.printf("Finished %d tests in %dms. The algorithms were run a a total of %d times.\n",
+                3 * POOLS.length, (end - start) / 1_000_000, 3 * REPEAT_TIMES * POOLS.length);
+
+        /* Graceful shutdown */
+        executorService.shutdown();
+        try {
+            executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
