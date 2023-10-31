a500f12b18b042bdf35935217415c89861a6bd0e
Filippo Piloni <filippo.piloni@usi.ch>
2023-10-31 12:18:50 +0100

    Added

--- Assignment1/exp01/Test.java
+++ Assignment1/exp01/Test.java
@@ -1,5 +1,7 @@
 package exp01;
 
+import java.io.File;
+import java.io.IOException; 
 import java.util.Arrays;
 import java.util.Random;
 
@@ -7,7 +9,7 @@ import javax.swing.Popup;
 
 public class Test {
 
-    private static int REPEAT_TIMES = 10; // Number of repetitions for a multiple benchmark test
+    private static int REPEAT_TIMES = 1000; // Number of repetitions for a multiple benchmark test
 
     /*
      * Generate a random Integer array of given size.
@@ -17,9 +19,27 @@ public class Test {
         Random random = new Random();
 
         for (int i = 0; i < size; i++) {
-            array[i] = random.nextInt(100); // 0-99
+            array[i] = random.nextInt(); // 0-99
         }
 
+        return array;
+    }
+
+    private static String[] generateRandomStringArray(int size) {
+        String[] array = new String[size];
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 10; //lenght of the string
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        Random random = new Random();
+        for(int i = 0; i < size; i++) {
+            for (int j = 0; j < targetStringLength; j++) {
+                int randomLimitedInt = leftLimit + (int) 
+                  (random.nextFloat() * (rightLimit - leftLimit + 1));
+                buffer.append((char) randomLimitedInt);
+            }
+            array[i] = buffer.toString();
+        }
         return array;
     }
 
@@ -41,6 +61,11 @@ public class Test {
      */
     private static <T extends Comparable<T>> long[] repeatBenchmark(Sorter<T> sorter, T[] array, int times) {
         long[] attempts = new long[times];
+        /*Warmup of 100 cycles */
+        for (int i = 0; i < 100; i++) {
+            T[] burnerArray = Arrays.copyOf(array, array.length);
+            attempts[i] = benchmarkSort(sorter, burnerArray);
+        }
 
         for (int i = 0; i < times; i++) {
             T[] burnerArray = Arrays.copyOf(array, array.length);
@@ -68,7 +93,7 @@ public class Test {
         BubbleSortUntilNoChange<Integer> uncSorter = new BubbleSortUntilNoChange<Integer>();
         BubbleSortWhileNeeded<Integer> wnSorter = new BubbleSortWhileNeeded<Integer>();
 
-        int[] pools = new int[] { 100, 1000, 10000, 100000, 1000000 }; // sizes of arrays to test
+        int[] pools = new int[] { 100, 1000, 10000,}; // sizes of arrays to test
         System.out.println("\nRUNING TEST ON ARRAYS OF VARYING SIZES");
         System.out.printf("Each test is performed %d times\n", REPEAT_TIMES);
         System.out.println("-".repeat(55));
@@ -88,7 +113,27 @@ public class Test {
             /* Print mean benchmarks */
             System.out.format("%10d%15d%15d%15d\n", p, meanTime(ppiAttempts), meanTime(uncAttempts),
                     meanTime(wnAttempts));
+            
         }
+        System.out.println("STRING SORTING");
+        System.out.println("-".repeat(55));
+        System.out.format("%10s%15s%15s%15s\n", "Array Size", "PassPerItem", "UntilNoChange", "WhileNeeded");
+        System.out.println("-".repeat(55));
+        // for (int p : pools) {
+        //     /* Generate random array of size p */
+        //     String[] array = generateRandomStringArray(p);
+
+        //     /* Implementation for multiple benchmarks */
+        //     long[] ppiAttempts = repeatBenchmark(ppiSorter, array, REPEAT_TIMES);
+        //     long[] uncAttempts = repeatBenchmark(uncSorter, array, REPEAT_TIMES);
+        //     long[] wnAttempts = repeatBenchmark(wnSorter, array, REPEAT_TIMES);
+
+        //     /* Print mean benchmarks */
+        //     System.out.format("%10d%15d%15d%15d\n", p, meanTime(ppiAttempts), meanTime(uncAttempts),
+        //             meanTime(wnAttempts));
+            
+        // }
+
         System.out.println("-".repeat(55));
     }
 }
