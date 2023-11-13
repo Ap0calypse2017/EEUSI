aa8262af62615872bda2be810f2f448abea39a1e
Alessandro Zanzi <alessandro.zanzi@usi.ch>
2023-11-13 09:59:43 +0100

    fixed file creation error

--- Assignment1/exp01/Test.java
+++ Assignment1/exp01/Test.java
@@ -2,10 +2,6 @@ package exp01;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
