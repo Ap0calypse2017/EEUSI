6be9e8d381f408b57d6155c1cd39bfd921bc156e
Filippo Piloni <filippo.piloni@usi.ch>
2023-11-16 12:37:24 +0100

    Continued report

--- Assignment1/graphmaker.py
+++ Assignment1/graphmaker.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import seaborn as sns
-df = pd.read_csv("Datas.csv")
+import matplotlib.pyplot as plt
+df = pd.read_csv("Datas/data_random.csv")
 dfInt = df[df.Type=="Integer"]
 dfByte = df[df.Type=="Byte"]
 dfString = df[df.Type=="String"]
@@ -8,7 +9,8 @@ dfString = df[df.Type=="String"]
 df100Int = dfInt[dfInt.Size==100]
 df1000Int = dfInt[dfInt.Size==1000]
 df5000Int = dfInt[dfInt.Size==5000]
-ax100Int = sns.catplot(data=df100Int, x="Algorithm", y="Time (nanoseconds)", kind="box")
+ax100Int = sns.catplot(data=df100Int, x="Algorithm", y="Time (nanoseconds)", kind="box").set(ylabel='Test')
+ax100Int.set(ylim=[0,1000000])
 ax1000Int = sns.catplot(data=df1000Int, x="Algorithm", y="Time (nanoseconds)", kind="box")
 ax5000Int = sns.catplot(data=df5000Int, x="Algorithm", y="Time (nanoseconds)", kind="box")
 
@@ -21,8 +23,10 @@ ax5000Byte = sns.catplot(data=df5000Byte, x="Algorithm", y="Time (nanoseconds)",
 
 df100String = dfString[dfString.Size==100]
 df1000String = dfString[dfString.Size==1000]
+df5000String = dfString[dfString.Size==5000]
 ax100String = sns.catplot(data=df100String, x="Algorithm", y="Time (nanoseconds)", kind="box")
 ax1000String = sns.catplot(data=df1000String, x="Algorithm", y="Time (nanoseconds)", kind="box")
+ax5000String = sns.catplot(data=df5000String, x="Algorithm", y="Time (nanoseconds)", kind="box")
 
 figureInt100 = ax100Int.fig
 figureInt1000 = ax1000Int.fig
@@ -34,14 +38,16 @@ figureByte5000 = ax5000Byte.fig
 
 figureString100 = ax100String.fig
 figureString1000 = ax1000String.fig
+figureString5000 = ax5000String.fig
 
-figureInt100.savefig("./graphs/figureInt100.png")
-figureInt1000.savefig("./graphs/figureInt1000.png")
-figureInt5000.savefig("./graphs/figureInt5000.png")
+figureInt100.savefig("./graphs/figureIntRandom100.png")
+figureInt1000.savefig("./graphs/figureIntRandom1000.png")
+figureInt5000.savefig("./graphs/figureIntRandom5000.png")
 
-figureByte100.savefig("./graphs/figureByte100.png")
-figureByte1000.savefig("./graphs/figureByte1000.png")
-figureByte5000.savefig("./graphs/figureByte5000.png")
+figureByte100.savefig("./graphs/figureByteRandom100.png")
+figureByte1000.savefig("./graphs/figureByteRandom1000.png")
+figureByte5000.savefig("./graphs/figureByteRandom5000.png")
 
-figureString100.savefig("./graphs/figureString100.png")
-figureString1000.savefig("./graphs/figureString1000.png")
+figureString100.savefig("./graphs/figureStringRandom100.png")
+figureString1000.savefig("./graphs/figureStringRandom1000.png")
+figureString5000.savefig("./graphs/figureStringRandom5000.png")
