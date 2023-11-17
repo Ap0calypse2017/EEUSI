b4eff13c0326e1f1925d043b9a0f0f191781a19a
Filippo Piloni <filippo.piloni@usi.ch>
2023-11-17 12:43:13 +0100

    Continued report

--- Assignment1/graphmakerRandom.py
+++ Assignment1/graphmakerRandom.py
@@ -9,24 +9,24 @@ dfString = df[df.Type=="String"]
 df100Int = dfInt[dfInt.Size==100]
 df1000Int = dfInt[dfInt.Size==1000]
 df5000Int = dfInt[dfInt.Size==5000]
-ax100Int = sns.catplot(data=df100Int, x="Algorithm", y="Time (nanoseconds)", kind="box").set(ylabel='Test')
+ax100Int = sns.catplot(data=df100Int, x="Algorithm", y="Time (nanoseconds)", kind="box", hue="Algorithm")
 ax100Int.set(ylim=[0,1000000])
-ax1000Int = sns.catplot(data=df1000Int, x="Algorithm", y="Time (nanoseconds)", kind="box")
-ax5000Int = sns.catplot(data=df5000Int, x="Algorithm", y="Time (nanoseconds)", kind="box")
+ax1000Int = sns.catplot(data=df1000Int, x="Algorithm", y="Time (nanoseconds)", kind="box", hue="Algorithm")
+ax5000Int = sns.catplot(data=df5000Int, x="Algorithm", y="Time (nanoseconds)", kind="box", hue="Algorithm")
 
 df100Byte = dfByte[dfByte.Size==100]
 df1000Byte = dfByte[dfByte.Size==1000]
 df5000Byte = dfByte[dfByte.Size==5000]
-ax100Byte = sns.catplot(data=df100Byte, x="Algorithm", y="Time (nanoseconds)", kind="box")
-ax1000Byte = sns.catplot(data=df1000Byte, x="Algorithm", y="Time (nanoseconds)", kind="box")
-ax5000Byte = sns.catplot(data=df5000Byte, x="Algorithm", y="Time (nanoseconds)", kind="box")
+ax100Byte = sns.catplot(data=df100Byte, x="Algorithm", y="Time (nanoseconds)", kind="box", hue="Algorithm")
+ax1000Byte = sns.catplot(data=df1000Byte, x="Algorithm", y="Time (nanoseconds)", kind="box", hue="Algorithm")
+ax5000Byte = sns.catplot(data=df5000Byte, x="Algorithm", y="Time (nanoseconds)", kind="box", hue="Algorithm")
 
 df100String = dfString[dfString.Size==100]
 df1000String = dfString[dfString.Size==1000]
 df5000String = dfString[dfString.Size==5000]
-ax100String = sns.catplot(data=df100String, x="Algorithm", y="Time (nanoseconds)", kind="box")
-ax1000String = sns.catplot(data=df1000String, x="Algorithm", y="Time (nanoseconds)", kind="box")
-ax5000String = sns.catplot(data=df5000String, x="Algorithm", y="Time (nanoseconds)", kind="box")
+ax100String = sns.catplot(data=df100String, x="Algorithm", y="Time (nanoseconds)", kind="box", hue="Algorithm")
+ax1000String = sns.catplot(data=df1000String, x="Algorithm", y="Time (nanoseconds)", kind="box", hue="Algorithm")
+ax5000String = sns.catplot(data=df5000String, x="Algorithm", y="Time (nanoseconds)", kind="box",)
 
 figureInt100 = ax100Int.fig
 figureInt1000 = ax1000Int.fig
