e97832fba14e3e3aea2d9b57063984c1f1a14aff
Filippo Piloni <filippo.piloni@usi.ch>
2023-11-19 18:02:09 +0100

    Added zip

---
+++ Assignment1/Assignment_1_Piloni_Zanzi/code/Test.java
@@ -0,0 +1,300 @@
+package exp01;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.function.Supplier;
+import java.util.Collections;
+
+public class Test {
+    private static int REPEAT_TIMES = 100; // Number of repetitions for a multiple benchmark test
+    private static int WARMUP_CYCLES = 25; // Cycles when warming up;
+    private static String OUTPUT_DIR = "./results/";
+    private static int STRING_LENGTH = 5;
+    private static int[] POOLS = { 100, 1000, 5000 }; // sizes of arrays to test
+
+    /*
+     * Generate a random Integer array.
+     */
+    private static Integer[] genRandIntArray(int size) {
+        Integer[] array = new Integer[size];
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt(); 
+        }
+        return array;
+    }
+
+    /*
+     * Generate an Integer array in ascending order.
+     */
+    private static Integer[] genAscIntArray(int size) {
+        Integer[] array = new Integer[size];
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt(); 
+        }
+        Arrays.sort(array);
+        return array;
+    }
+
+    /*
+     * Generate an Integer array in descending order.
+     */
+    private static Integer[] genDescIntArray(int size) {
+        Integer[] array = new Integer[size];
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt(); 
+        }
+        Arrays.sort(array, Collections.reverseOrder());
+        return array;
+    }
+
+    /*
+     * Generate a random String array.
+     */
+    private static String[] genRandStrArray(int size) {
+        String[] array = new String[size];
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        StringBuilder buffer = new StringBuilder(STRING_LENGTH);
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < STRING_LENGTH; j++) {
+                int randomLimitedInt = leftLimit + (int) (random.nextFloat() * (rightLimit - leftLimit + 1));
+                buffer.append((char) randomLimitedInt);
+            }
+            array[i] = buffer.toString();
+        }
+        return array;
+    }
+
+    /*
+     * Generate a String array in ascending order.
+     */
+    private static String[] genAscStrArray(int size) {
+        String[] array = new String[size];
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        StringBuilder buffer = new StringBuilder(STRING_LENGTH);
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < STRING_LENGTH; j++) {
+                int randomLimitedInt = leftLimit + (int) (random.nextFloat() * (rightLimit - leftLimit + 1));
+                buffer.append((char) randomLimitedInt);
+            }
+            array[i] = buffer.toString();
+        }
+        Arrays.sort(array);
+
+        return array;
+    }
+
+    /*
+     * Generate a String array in descending order.
+     */
+    private static String[] genDescStrArray(int size) {
+        String[] array = new String[size];
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        StringBuilder buffer = new StringBuilder(STRING_LENGTH);
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < STRING_LENGTH; j++) {
+                int randomLimitedInt = leftLimit + (int) (random.nextFloat() * (rightLimit - leftLimit + 1));
+                buffer.append((char) randomLimitedInt);
+            }
+            array[i] = buffer.toString();
+        }
+        Arrays.sort(array, Collections.reverseOrder());
+
+        return array;
+
+    }
+
+    /*
+     * Generate a random Byte array.
+     */
+    private static Byte[] genRandByteArray(int size) {
+        Byte[] array = new Byte[size];
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            array[i] = (byte) (random.nextInt(256) - 128);
+        }
+        return array;
+    }
+
+    /*
+     * Generate a Byte array in ascending order.
+     */
+    private static Byte[] genAscByteArray(int size) {
+        Byte[] array = new Byte[size];
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            array[i] = (byte) (random.nextInt(256) - 128);
+        }
+        // Sort the array
+        Arrays.sort(array);
+        return array;
+    }
+
+    /*
+     * Generate a Byte array in descending order.
+     */
+    private static Byte[] genDescByteArray(int size) {
+        Byte[] array = new Byte[size];
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            array[i] = (byte) (random.nextInt(256) - 128);
+        }
+        // Sort the array in reverse order
+        Arrays.sort(array, Collections.reverseOrder());
+        return array;
+    }
+
+    
+
+    /*
+     * Compute time(ns) taken for a sorter to sort an array.
+     */
+    private static <T extends Comparable<T>> long benchmarkSort(Sorter<T> sorter, T[] array) {
+        long start, end;
+        start = System.nanoTime();
+        sorter.sort(array);
+        end = System.nanoTime();
+
+        return end - start;
+    }
+
+    /*
+     * Warmups.
+     */
+    private static <T extends Comparable<T>> void warmup(Sorter<T> sorter, T[] array, int cycles) {
+        for (int i = 0; i < cycles; i++) {
+            benchmarkSort(sorter, cpArray(array));
+        }
+    }
+
+    /*
+     * Compute the mean value given an array of longs.
+     */
+    private static long meanTime(long[] attempts) {
+        long sum = 0;
+        for (long t : attempts) {
+            sum += t;
+        }
+
+        return sum / attempts.length;
+    }
+
+    /*
+     * Prints on terminal the benchmark header
+     */
+    private static void printHeader() {
+        System.out.println("\nRUNING TEST ON ARRAYS OF VARYING SIZES");
+        System.out.printf("Each test is performed %d times\n", REPEAT_TIMES);
+        System.out.println("-".repeat(65));
+        System.out.format("%-10s%10s%15s%15s%15s\n", "Type", "Size", "PassPerItem", "UntilNoChange",
+                "WhileNeeded");
+        System.out.println("-".repeat(65));
+    }
+
+    /*
+     * Prints on terminal a table row of results
+     */
+    private static void printBenchResult(String type, int pool, long[] ppi, long[] unc, long[] wn) {
+        System.out.format("%-10s%10d%15d%15d%15d\n", type, pool, meanTime(ppi), meanTime(unc), meanTime(wn));
+    }
+
+    /*
+     * Write array to file
+     */
+    public static void writeToFile(long array[], String filename) {
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter(OUTPUT_DIR + filename);
+            for (long time : array) {
+                writer.write(time + System.lineSeparator());
+            }
+
+            writer.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /*
+     * Duplicate an array.
+     */
+    public static <T> T[] cpArray(T[] array) {
+        return Arrays.copyOf(array, array.length);
+    }
+
+    /*
+     * Perform a full cycle of benchmarks
+     */
+    public static <T extends Comparable<T>> void benchmark(BubbleSortPassPerItem<T> ppiSorter,
+            BubbleSortUntilNoChange<T> uncSorter,
+            BubbleSortWhileNeeded<T> wnSorter, Supplier<T[]> arrayGenerator, int pool, String type) {
+
+        long[] ppiAttempts = new long[REPEAT_TIMES];
+        long[] uncAttempts = new long[REPEAT_TIMES];
+        long[] wnAttempts = new long[REPEAT_TIMES];
+
+        for (int i = 0; i < REPEAT_TIMES; i++) {
+            T[] array = arrayGenerator.get();
+            ppiAttempts[i] = benchmarkSort(ppiSorter, cpArray(array));
+            uncAttempts[i] = benchmarkSort(uncSorter, cpArray(array));
+            wnAttempts[i] = benchmarkSort(wnSorter, cpArray(array));
+        }
+
+        writeToFile(ppiAttempts, type + "_ppi_" + pool + ".txt");
+        writeToFile(uncAttempts, type + "_unc_" + pool + ".txt");
+        writeToFile(wnAttempts, type + "_wn_" + pool + ".txt");
+
+        printBenchResult(type, pool, ppiAttempts, uncAttempts, wnAttempts);
+    }
+
+    public static void main(String args[]) {
+        /* Initialize sorters */
+        BubbleSortPassPerItem<String> ppiSorterStr = new BubbleSortPassPerItem<String>();
+        BubbleSortUntilNoChange<String> uncSorterStr = new BubbleSortUntilNoChange<String>();
+        BubbleSortWhileNeeded<String> wnSorterStr = new BubbleSortWhileNeeded<String>();
+
+        // BubbleSortPassPerItem<Double> ppiSorterDbl = new BubbleSortPassPerItem<Double>();
+        // BubbleSortUntilNoChange<Double> uncSorterDbl = new BubbleSortUntilNoChange<Double>();
+        // BubbleSortWhileNeeded<Double> wnSorterDbl = new BubbleSortWhileNeeded<Double>();
+
+        BubbleSortPassPerItem<Integer> ppiSorterInt = new BubbleSortPassPerItem<Integer>();
+        BubbleSortUntilNoChange<Integer> uncSorterInt = new BubbleSortUntilNoChange<Integer>();
+        BubbleSortWhileNeeded<Integer> wnSorterInt = new BubbleSortWhileNeeded<Integer>();
+
+        BubbleSortPassPerItem<Byte> ppiSorterByte = new BubbleSortPassPerItem<Byte>();
+        BubbleSortUntilNoChange<Byte> uncSorterByte = new BubbleSortUntilNoChange<Byte>();
+        BubbleSortWhileNeeded<Byte> wnSorterByte = new BubbleSortWhileNeeded<Byte>();
+
+        printHeader();
+
+        warmup(ppiSorterInt, genRandIntArray(750), WARMUP_CYCLES); // correct way of warming up here?
+
+        /* Go through all test cases */
+        for (int p : POOLS) {
+            benchmark(ppiSorterInt, uncSorterInt, wnSorterInt, () -> genRandIntArray(p), p, "Integer~");
+            benchmark(ppiSorterInt, uncSorterInt, wnSorterInt, () -> genAscIntArray(p), p, "Integer>");
+            benchmark(ppiSorterInt, uncSorterInt, wnSorterInt, () -> genDescIntArray(p), p, "Integer<");
+
+            benchmark(ppiSorterByte, uncSorterByte, wnSorterByte, () -> genRandByteArray(p), p, "Byte~");
+            benchmark(ppiSorterByte, uncSorterByte, wnSorterByte, () -> genAscByteArray(p), p, "Byte>");
+            benchmark(ppiSorterByte, uncSorterByte, wnSorterByte, () -> genDescByteArray(p), p, "Byte<");
+
+            benchmark(ppiSorterStr, uncSorterStr, wnSorterStr, () -> genRandStrArray(p), p, "String~");
+            benchmark(ppiSorterStr, uncSorterStr, wnSorterStr, () -> genAscStrArray(p), p, "String>");
+            benchmark(ppiSorterStr, uncSorterStr, wnSorterStr, () -> genDescStrArray(p), p, "String<");
+        }
+
+        System.out.println("-".repeat(65));
+    }
+}
---
+++ Assignment1/Assignment_1_Piloni_Zanzi/graphmakerRandom.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+df = pd.read_csv("Datas/data_random.csv")
+dfInt = df[df.Type=="Integer"]
+dfByte = df[df.Type=="Byte"]
+dfString = df[df.Type=="String"]
+
+df100Int = dfInt[dfInt.Size==100]
+df1000Int = dfInt[dfInt.Size==1000]
+df5000Int = dfInt[dfInt.Size==5000]
+ax100Int = sns.catplot(data=df100Int, x="Algorithm", y="Time (nanoseconds)", kind="box", hue="Algorithm")
+ax100Int.set(ylim=[0,1000000])
+ax1000Int = sns.catplot(data=df1000Int, x="Algorithm", y="Time (nanoseconds)", kind="box", hue="Algorithm")
+ax5000Int = sns.catplot(data=df5000Int, x="Algorithm", y="Time (nanoseconds)", kind="box", hue="Algorithm")
+
+df100Byte = dfByte[dfByte.Size==100]
+df1000Byte = dfByte[dfByte.Size==1000]
+df5000Byte = dfByte[dfByte.Size==5000]
+ax100Byte = sns.catplot(data=df100Byte, x="Algorithm", y="Time (nanoseconds)", kind="box", hue="Algorithm")
+ax1000Byte = sns.catplot(data=df1000Byte, x="Algorithm", y="Time (nanoseconds)", kind="box", hue="Algorithm")
+ax5000Byte = sns.catplot(data=df5000Byte, x="Algorithm", y="Time (nanoseconds)", kind="box", hue="Algorithm")
+
+df100String = dfString[dfString.Size==100]
+df1000String = dfString[dfString.Size==1000]
+df5000String = dfString[dfString.Size==5000]
+ax100String = sns.catplot(data=df100String, x="Algorithm", y="Time (nanoseconds)", kind="box", hue="Algorithm")
+ax1000String = sns.catplot(data=df1000String, x="Algorithm", y="Time (nanoseconds)", kind="box", hue="Algorithm")
+ax5000String = sns.catplot(data=df5000String, x="Algorithm", y="Time (nanoseconds)", kind="box",)
+
+figureInt100 = ax100Int.fig
+figureInt1000 = ax1000Int.fig
+figureInt5000 = ax5000Int.fig
+
+figureByte100 = ax100Byte.fig
+figureByte1000 = ax1000Byte.fig
+figureByte5000 = ax5000Byte.fig
+
+figureString100 = ax100String.fig
+figureString1000 = ax1000String.fig
+figureString5000 = ax5000String.fig
+
+figureInt100.savefig("./graphs/figureIntRandom100.png")
+figureInt1000.savefig("./graphs/figureIntRandom1000.png")
+figureInt5000.savefig("./graphs/figureIntRandom5000.png")
+
+figureByte100.savefig("./graphs/figureByteRandom100.png")
+figureByte1000.savefig("./graphs/figureByteRandom1000.png")
+figureByte5000.savefig("./graphs/figureByteRandom5000.png")
+
+figureString100.savefig("./graphs/figureStringRandom100.png")
+figureString1000.savefig("./graphs/figureStringRandom1000.png")
+figureString5000.savefig("./graphs/figureStringRandom5000.png")
